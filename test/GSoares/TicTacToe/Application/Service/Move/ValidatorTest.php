625f117c55a9dffdadbc134536791d7b2c67e54b
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2017-05-07 10:58:00 -0300

    improve unit tests

---
+++ test/GSoares/TicTacToe/Application/Service/Move/ValidatorTest.php
@@ -0,0 +1,83 @@
+<?php
+namespace GSoares\TicTacToe\Application\Service\Move;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @author Gabriel Felipe Soares <gabrielfs7@gmail.com>
+ */
+class ValidatorTest extends TestCase
+{
+
+    /**
+     * @var Validator|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $validator;
+
+    public function setUp()
+    {
+        $this->validator = new Validator();
+
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        $this->validator = null;
+
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     * @expectedMessage Syntax Error
+     */
+    public function testInvalidJsonMustThrowException()
+    {
+        $this->validator->validateMoveByRequest(new Request());
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     * @expectedMessage boardState and playerUnit must me provided
+     */
+    public function testMissingRequiredJsonMustThrowException()
+    {
+        $this->validator->validateMoveByRequest(
+            new Request(
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                '{}'
+            )
+        );
+    }
+    
+    /**
+     * @test
+     */
+    public function testReturnDtoIfIsAValidJson()
+    {
+        $expectedDto = new \stdClass();
+        $expectedDto->boardState = [];
+        $expectedDto->playerUnit = 'X';
+
+        $request = new Request(
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            '{ "boardState" : [], "playerUnit" : "X" }'
+        );
+
+        $this->assertEquals($expectedDto, $this->validator->validateMoveByRequest($request));
+    }
+}
